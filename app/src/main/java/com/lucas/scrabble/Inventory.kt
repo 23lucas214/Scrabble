f324610c2c8dfdf3700875c4e6b89ec8ee84f7a4
Brichard <baptisterichard.centrale@gmail.com>
2024-02-09 17:17:30 +0100

    09/02

--- app/src/main/java/com/lucas/scrabble/Inventory.kt
+++ app/src/main/java/com/lucas/scrabble/Inventory.kt
@@ -1,53 +1,91 @@
 package com.lucas.scrabble
 
 import kotlin.random.Random
-import kotlin.random.nextInt
 
 class Inventory {
+    private var score : Int = 0
+    private var scrabble : Boolean = false //remettre false apres chaque tour de jeu
 
     /**
      * Main d'un joueur
      */
-    private val main = mutableListOf<String>()
+    private var main = mutableListOf<String>()
 
-    fun addNLetter(n: Int){
+    /**
+     * Tirer n lettres et les ajouter a la main
+     */
+    fun tirerNLettres(n: Int, L: MutableList<String>){
         for (i in 1..n){
-            main.add(tirer())
+            main.add(tirer(L))
         }
     }
 
-    fun tirer() : String{
-        val alea = Random.nextInt(101) + 1
-        val lettre  = when {
-            9 >= alea -> "A"
-            11 >= alea -> "B"
-            13 >= alea -> "C"
-            16 >= alea -> "D"
-            31 >= alea -> "E"
-            33 >= alea -> "F"
-            35 >= alea -> "G"
-            37 >= alea -> "H"
-            45 >= alea -> "I"
-            46 >= alea -> "J"
-            47 >= alea -> "K"
-            52 >= alea -> "L"
-            55 >= alea -> "M"
-            61 >= alea -> "N"
-            67 >= alea -> "O"
-            69 >= alea -> "P"
-            70 >= alea -> "Q"
-            76 >= alea -> "R"
-            82 >= alea -> "S"
-            88 >= alea -> "T"
-            94 >= alea -> "U"
-            96 >= alea -> "V"
-            97 >= alea -> "W"
-            98 >= alea -> "X"
-            99 >= alea -> "Y"
-            100 >= alea -> "Z"
-            102 >= alea -> " "
-            else -> "err"
-        }
+    /**
+     * Tirer une lettre au hasard dans la pioche
+     */
+    fun tirer(pioche: MutableList<String>) : String{
+        val alea = Random.nextInt(pioche.size) + 1
+        var lettre : String = pioche.removeAt(alea)
         return lettre
+    }
+
+    fun calculPointsMot(mot: Pair<String,MutableList<Pair<Int,Int>>>, plateauDebutTour: Plateau, jeu : Jeu): Int{
+        var scoreMot = 0
+        var multiplicateurLettre = 1
+        var multiplicateurMot = 1
+        var contenu: String
+        var position: Pair<Int,Int>
+        for (k in 1..mot.second.size-1) {
+            position = mot.second.get(k)
+            contenu = plateauDebutTour.getCase(position)
+            if ((contenu != "2M") && (contenu != "3M")) {
+                multiplicateurLettre = when {
+                    contenu == "2L" -> 2
+                    contenu == "3L" -> 3
+                    else -> 1
+                }
+            } else if (contenu == "2M") {
+                multiplicateurMot= 2
+            } else {
+                multiplicateurMot = 3
+            }
+            scoreMot += multiplicateurLettre * jeu.lettrePoints(mot.first[k].toString())
+            multiplicateurLettre = 1
+        }
+        score += scoreMot * multiplicateurMot
+        return scoreMot * multiplicateurMot
+        //Scrabble
+    }
+
+    /**
+     * Fonction qui renvoie la liste des mots ajoutes durant un tour
+     */
+    fun motsAjoutes(plateauDebutTour: Plateau, plateauFinTour: Plateau): MutableList<Pair<String, MutableList<Pair<Int, Int>>>>{
+        var list = plateauFinTour.listeDeMots()
+        list.removeAll(plateauDebutTour.listeDeMots())
+        return list
+    }
+
+
+    fun poserLettre(index: Int, plateau: Plateau, position: Pair<Int,Int>){
+        plateau.setCase(position, main.removeAt(index))
+    }
+
+    fun recupererLettre(position: Pair<Int,Int>, plateauDebutTour: Plateau, plateauActu: Plateau){
+        if(plateauActu.getCase(position) != plateauDebutTour.getCase(position)) {
+            main.add(plateauActu.getCase(position))
+            plateauActu.setCase(position, plateauDebutTour.getCase(position))
+        }
+    }
+
+    fun deplacerLettreInv(index1: Int, index2: Int){
+        val l = main.get(index1)
+        main.set(index1, main.get(index2))
+        main.set(index2, l)
+    }
+    fun echangeDeLettre(index: Int, pioche: MutableList<String>){
+        val tempo = main.removeAt(index)
+        tirerNLettres(1, pioche)
+        pioche.add(tempo)
     }
 }
--- app/src/main/java/com/lucas/scrabble/Plateau.kt
+++ app/src/main/java/com/lucas/scrabble/Plateau.kt
@@ -1,6 +1,15 @@
 package com.lucas.scrabble
 
 class Plateau {
+
+    /**
+     * Plateau de jeu initial
+     * CD est la case sur laquelle poser le premier mot
+     * 0 les cases classiques
+     * 2 pour double, 3 pour triple
+     * M si c'est le score du mot qui est multiplié
+     * L si c'est celui de la lettre
+     */
     private var plateau = arrayOf(
         arrayOf("3M", "0", "0", "2L", "0", "0", "0", "2M", "0", "0", "0", "2L", "0", "0", "3M"),
         arrayOf("0", "2M", "0", "0", "0", "3L", "0", "0", "0", "3L", "0", "0", "0", "2M", "0"),
@@ -8,7 +17,7 @@ class Plateau {
         arrayOf("2L", "0", "0", "3L", "0", "0", "0", "2L", "0", "0", "0", "2M", "0", "0", "2L"),
         arrayOf("0", "0", "0", "0", "2M", "0", "0", "0", "0", "0", "2M", "0", "0", "0", "0"),
         arrayOf("0", "3L", "0", "0", "0", "3L", "0", "0", "0", "3L", "0", "0", "0", "3L", "0"),
-        arrayOf("0", "0", "2L", "0", "0", "0", "2L", "0", "2L", "0", "0", "0", "2L", "0"),
+        arrayOf("0", "0", "2L", "0", "0", "0", "2L", "0", "2L", "0", "0", "0", "2L", "0", "0"),
         arrayOf("3M", "0", "0", "2L", "0", "0", "0", "CD", "0", "0", "0", "2L", "0", "0", "3M"),
         arrayOf("0", "0", "2L", "0", "0", "0", "2L", "0", "2L", "0", "0", "0", "2L", "0", "0"),
         arrayOf("0", "3L", "0", "0", "0", "3L", "0", "0", "0", "3L", "0", "0", "0", "3L", "0"),
@@ -19,41 +28,84 @@ class Plateau {
         arrayOf("3M", "0", "0", "2L", "0", "0", "0", "3M", "0", "0", "0", "2L", "0", "0", "3M"),
     )
 
+    /**
+     * Liste des cases sur lesquelles on peut ecrire
+     */
     val collisionOk = listOf("0", "2L", "3L", "2M", "3M", "CD")
 
+    /**
+     * Verifier si une case est toujours disponible
+     */
     fun verifCaseLibre(i: Int, j: Int) : Boolean{
         return plateau[i][j] in collisionOk
     }
 
-    fun listeDeMots() : List<String>{
-        var list = mutableListOf<String>()
+    /**
+     * Renvoie une liste contenant les mots ecris sur le plateau
+     */
+    fun listeDeMots() : MutableList<Pair<String, MutableList<Pair<Int, Int>>>>{
+        var list = mutableListOf<Pair<String, MutableList<Pair<Int, Int>>>>()
         var mot : String = ""
+        var coord = mutableListOf<Pair<Int, Int>>()
         for (i in 0..14){
             for (j in 0..14){
                 if (verifCaseLibre(i, j)){
-                    if (mot.length>1){list.add(mot)}
+                    if (mot.length>1){list.add(Pair(mot, coord))}
                     mot = ""
+                    coord = mutableListOf<Pair<Int, Int>>()
                 }
                 else{
                     mot += plateau[i][j]
+                    coord.add(Pair(i, j))
+
                 }
             }
-            if (mot.length>1){list.add(mot)}
+            if (mot.length>1){list.add(Pair(mot, coord))}
         }
 
         mot = ""
+        coord = mutableListOf<Pair<Int, Int>>()
         for (i in 0..14){
             for (j in 0..14){
                 if (verifCaseLibre(j, i)){
-                    if (mot.length>1){list.add(mot)}
+                    if (mot.length>1){list.add(Pair(mot, coord))}
                     mot = ""
+                    coord = mutableListOf<Pair<Int, Int>>()
                 }
                 else{
                     mot += plateau[j][i]
+                    coord.add(Pair(i, j))
                 }
             }
-            if (mot.length>1){list.add(mot)}
+            if (mot.length>1){list.add(Pair(mot, coord))}
         }
         return list
     }
+
+    /**
+     * Renvoie la case correspondante aux coordonnées
+     */
+    fun getCase(coord: Pair<Int, Int>): String{
+        return plateau[coord.first][coord.second]
+    }
+
+    fun setCase(coord: Pair<Int, Int>, case: String){
+        plateau[coord.first][coord.second] = case
+    }
+
+    /**
+     * Verifie que tous les mots formés sur le plateau sont acceptables
+     * Attention le dictionnaire doit etre initialise
+     */
+    fun verifierPlateau(jeu: Jeu): Boolean{
+        val mots = listeDeMots()
+        var bool = true
+        for (x in mots){
+            if (!jeu.dictionnaire.contains(x.first)){
+                bool = false
+            }
+        }
+        return false
+    }
+
 }
